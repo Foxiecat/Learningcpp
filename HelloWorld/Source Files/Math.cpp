c7d39277ab8e0695bf687b2bef3ed44b4b399809
Foxiecat <foxiecat+programming@pm.me>
2025-07-27 12:30:38 +0200

    #include literally just pastes in the relevant code.

---
+++ HelloWorld/Source Files/Math.cpp
@@ -0,0 +1,10 @@
+const char* Logr(const char* message)
+{
+    return message;
+}
+
+int Multiply(const int a, const int b)
+{
+    Logr("Multiply");
+    return a * b;
+#include "../Header Files/EndBrace.h"
